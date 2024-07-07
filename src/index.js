a080aeed23b22da1c44b798d353711cd75b4d930
Emmaculate <emmamwitimi14@gmail.com>
2024-07-07 12:51:48 +0300

    to do list application

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // references to the form and the tasks list
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+  //eventListener to our form
+  form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    //get the value of taskdescription input
+    const taskdescription = document.getElementById("new-task-description");
+
+    //new li item to set the taskdescription content
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskdescription;
+
+    //append taskitem to tasks list
+    taskList.appendChild(taskItem);
+
+    //clear the input field after submitting
+    document.getElementById("new-task-description").value = "";
+  });
+
 });
